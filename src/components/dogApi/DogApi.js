e54c07d20b93039794153bdc94b4151309b813b8
josielsifa <josielsifa@gmail.com>
2019-10-04 20:04:21 -0300

    Ajustando select com json

--- src/components/dogApi/DogApi.js
+++ src/components/dogApi/DogApi.js
@@ -3,14 +3,16 @@ import React, { Component } from "react";
 class DogApi extends Component {
   constructor(props) {
     super(props);
-    this.state = { listDogAPI: []};
+    this.state = { listDogAPI: [] };
     console.log(this.getDogApiLIst());
 
     this.getDogApiLIst = this.getDogApiLIst.bind(this);
   }
 
   getDogApiLIst() {
+    let dog_img = `akita`;
     let url_coversor = `https://dog.ceo/api/breeds/list/all`;
+    // let url_coversor = `https://dog.ceo/api/breed/${dog_img}/images/random`;
 
     fetch(url_coversor)
       .then(res => {
@@ -18,24 +20,46 @@ class DogApi extends Component {
       })
       .then(json => {
         try {
-          console.log(json.message);
-          let listDogAPI = json;
+          let listDogAPI = [json.message];
+
+          // let asd = JSON.parse(listDogAPI);
+          console.log(listDogAPI);
           this.setState({ listDogAPI });
         } catch (error) {
-          console.log(json.error);
-          alert(json.error);
+          console.log(error);
+          alert(error);
         }
       })
       .catch(error => {
         console.log(error);
         alert(error);
       });
+
   }
+
+
 
   render() {
     return (
       <div className="list-dog-api">
-        {this.state.listDogAPI.map(home => <div>{home}</div>)}
+        {/* {
+          this.state.listDogAPI.map(key => (
+            <select key={key}>
+              {this.state.data.map(({ [key]: value }) => <option key={value}>{value}</option>)}
+            </select>
+          ))} */}
+
+        {
+          this.state.listDogAPI.map(((cloud, index) =>
+            console.log(cloud)
+            // <th key={`${cloud}`}>
+            //   <div>teste</div>
+            // </th>
+            // < select key={`${cloud.cloud}${index}`}>
+
+            // </select>
+          ))
+        }
       </div>
     );
   }
