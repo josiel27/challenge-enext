5caa011bae7f5a84477d5f57d7dd1c077aea6978
josielsifa <josielsifa@gmail.com>
2019-10-06 00:51:04 -0300

    Updates

---
+++ precache-manifest.6776c423f6074b2f7efac4885dfad044.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2308c785123a83f74ead7ab5e336675f",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "042c7a01c58bd43f9b29",
+    "url": "/react-gh-pages/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "f0de605a17ba148fa71f",
+    "url": "/react-gh-pages/static/css/main.aa9e78d4.chunk.css"
+  },
+  {
+    "revision": "042c7a01c58bd43f9b29",
+    "url": "/react-gh-pages/static/js/2.2071c0b2.chunk.js"
+  },
+  {
+    "revision": "f0de605a17ba148fa71f",
+    "url": "/react-gh-pages/static/js/main.bc2c83c5.chunk.js"
+  },
+  {
+    "revision": "49acaaa445f5b76c1bea",
+    "url": "/react-gh-pages/static/js/runtime-main.f2ba38dd.js"
+  },
+  {
+    "revision": "22b3170c9e556fd13a1eb8f012a5635d",
+    "url": "/react-gh-pages/static/media/Acme-Regular.22b3170c.ttf"
+  },
+  {
+    "revision": "10c6818802ed96f9d6976633ae148c37",
+    "url": "/react-gh-pages/static/media/Anton-Regular.10c68188.ttf"
+  },
+  {
+    "revision": "8e96daaaad1703684a9d7011f9fbe90a",
+    "url": "/react-gh-pages/static/media/JosefinSans-Regular.8e96daaa.ttf"
+  },
+  {
+    "revision": "dc386f0b36c8cfdbe2f0efbbb5c83781",
+    "url": "/react-gh-pages/static/media/Notable-Regular.dc386f0b.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/react-gh-pages/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "319bc19ddb21acade36b8dab760e9778",
+    "url": "/react-gh-pages/static/media/defaultDog.319bc19d.jpg"
+  }
+]);
